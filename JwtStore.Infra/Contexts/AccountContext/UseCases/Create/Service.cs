f4aea93011ef6adda55defab5b6554cb0b40b692
Igor Santiago <contato.igorsantiago@outlook.com>
2023-09-12 17:03:25 -0300

    Infra: Implement Repository and Service UseCase

---
+++ JwtStore.Infra/Contexts/AccountContext/UseCases/Create/Service.cs
@@ -0,0 +1,21 @@
+﻿using JwtStore.Core;
+using JwtStore.Core.Contexts.AccountContext.Entities;
+using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+
+namespace JwtStore.Infra.Contexts.AccountContext.UseCases.Create;
+
+public class Service : IService
+{
+    public async Task SendVerificationEmailAsync(User user, CancellationToken cancellationToken)
+    {
+        var client = new SendGridClient(Configuration.SendGrid.ApiKey);
+        var from = new EmailAddress(Configuration.Email.DefaultFromEmail, Configuration.Email.DefaultFromName);
+        const string subject = "Verify your account";
+        var to = new EmailAddress(user.Email, user.Name);
+        var content = $"Code: {user.Email.Verification.Code}";
+        var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
+        await client.SendEmailAsync(msg, cancellationToken);
+    }
+}
