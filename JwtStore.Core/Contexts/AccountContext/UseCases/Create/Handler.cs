d4e192ff968a61e1a059487e1f81562ac880c1a2
Igor Santiago <contato.igorsantiago@outlook.com>
2023-09-12 15:34:44 -0300

    Core: Create UseCase for creating account

---
+++ JwtStore.Core/Contexts/AccountContext/UseCases/Create/Handler.cs
@@ -0,0 +1,97 @@
+﻿using JwtStore.Core.Contexts.AccountContext.Entities;
+using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts;
+using JwtStore.Core.Contexts.AccountContext.ValueObjects;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create;
+
+public class Handler
+{
+    private readonly IRepository _repository;
+    private readonly IService _service;
+
+    public Handler(IRepository repository, IService service)
+    {
+        _repository = repository;
+        _service = service;
+    }
+
+    public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
+    {
+        #region Request Validation
+
+        try
+        {
+            var response = Specification.Ensure(request);
+            if (!response.IsValid)
+                return new Response("Invalid Request", 400, response.Notifications);
+        }   
+        catch
+        {
+            return new Response("Unable to validate your request", 500);
+        }
+
+        #endregion
+
+        #region Create Object
+
+        Email email;
+        Password password;
+        User user;
+
+        try
+        {
+            email = new(request.Email);
+            password = new(request.Password);
+            user = new(request.Name, email, password);
+        }
+        catch (Exception ex)
+        {
+            return new Response(ex.Message, 400);
+        }
+
+        #endregion
+
+        #region Verify User
+
+        try
+        {
+            var exists = await _repository.AnyAsync(request.Email, cancellationToken);
+            if (exists)
+                return new Response("E-mail already exists", 400);
+        }
+        catch
+        {
+            return new Response("Failed to verify registered e-mail", 500);
+        }
+
+        #endregion
+
+        #region Data persistence
+
+        try
+        {
+            await _repository.SaveAsync(user, cancellationToken);
+        }
+        catch
+        {
+            return new Response("Failed in data persistence", 500);
+        }
+
+        #endregion
+
+        #region Send activation e-mail
+
+        try
+        {
+            await _service.SendVerificationEmailAsync(user, cancellationToken);
+        }
+        catch
+        {
+            // Already persisted data. Do nothing.
+        }
+
+        #endregion
+
+        return new Response("Account created successfully", new ResponseData(user.Id, user.Name, user.Email));
+    }
+}
---
+++ JwtStore.Core/Contexts/AccountContext/UseCases/Create/Specification.cs
@@ -0,0 +1,15 @@
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create;
+
+public static class Specification
+{
+    public static Contract<Notification> Ensure(Request request) => new Contract<Notification>()
+        .Requires()
+        .IsLowerThan(request.Name.Length, 120, "Name", "The maximum length of a Name is 120 characters")
+        .IsGreaterOrEqualsThan(request.Name.Length, 3, "Name", "The minimum length of a Name is 3 characters")
+        .IsLowerThan(request.Password.Length, 128, "The maximum length of a Password is 128 characters")
+        .IsGreaterOrEqualsThan(request.Password.Length, 8, "The minimum length of a Password is 8 characters")
+        .IsEmail(request.Email, "Email", "Invalid e-mail!");
+}
